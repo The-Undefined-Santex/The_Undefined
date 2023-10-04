4716ec91780cfb5a47990b554521cc4f91c2c426
Exequiel <eportero421@gmail.com>
2023-10-04 20:39:09 -0300

    chore: Add contact information model
    Add a contact information model relationated with student model

---
+++ front/src/app/core/model/contact-information.model.ts
@@ -0,0 +1,11 @@
+export class ContactInformation {
+
+    public phone_number: string = ''
+    public country: string = ''
+    public state: string = ''
+    public address: string = ''
+    public email: string = '' 
+
+    constructor(){}
+
+}
