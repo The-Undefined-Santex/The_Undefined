95bff0f1e5bfba8a4cad0fc323814747fc4d5036
Arguello Soledad <soledad.ar1@gmail.com>
2023-10-11 20:30:05 -0300

    fix: correct variable in teacher

--- front/src/app/core/model/teachers.model.ts
+++ front/src/app/core/model/teachers.model.ts
@@ -7,34 +7,5 @@ export class Teachers{
    lastName: string = ''
    birth_date: Date = new Date()
    ContactInformation: ContactInformation = new ContactInformation()
-//  constructor(
-//     public id: number = 0,
-//     public dni: number = 0,
-//     public firstName: string = '',
-//     public lastName: string = '',
-//     public birth_date: Date = new Date(),
-//     public ContactInformation: ContactInformation = new ContactInformation(),
-//  ){}
-
-// public get _id() {
-//     return this.id
-// }
-
-// public get _dni() {
-//     return this.dni
-
-// }
-
-// public get _firstName() {
-//     return this.firstName
-
-// }
-
-// public get _lastName() {
-//     return this.lastName
-// }
-
-// public get _birth_date() {
-//     return this.birth_date
-// }
+   contactInformationId: number = 0
 }
