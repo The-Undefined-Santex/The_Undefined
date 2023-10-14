93dd512919862ab3cc7847ec4be20da663af34bc
Ludmimar <ludmilamartos@gmail.com>
2023-10-14 19:01:19 -0300

    feat(frontend):Modify courses.model.ts

--- front/src/app/core/model/courses.model.ts
+++ front/src/app/core/model/courses.model.ts
@@ -1,12 +1,19 @@
+import { CoursesDetailModel } from "./coursesDetails.model"
+
 export class CoursesModel {
-
-    id?: number
-    title: string
-
-    constructor(
-        title: string
-    ){
-        this.title = title
-    }
-
+    id?: number;
+    title: string = ""
+    subtitle: string = ""
+    description: string = ""
+    duration: number = 0
+    start_date: string = ""
+    capacity: number = 0
+    price: number = 0
+    due: number = 0
+    has_surcharge: number = 0
+    surcharge_percentage: number = 0
+    modality:string = ""
+    schedules:string = ""
+    banner:string = ""
+    courseDetails: CoursesDetailModel[] = []
 }
