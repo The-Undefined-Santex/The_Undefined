765c22634b585d418f8d182422e3b45bde5385ec
Ludmimar <ludmilamartos@gmail.com>
2023-10-16 13:06:20 -0300

    Update components.module.ts

--- front/src/app/components/components.module.ts
+++ front/src/app/components/components.module.ts
@@ -41,16 +41,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormInscrCoursesComponent } from './courses/form-inscr-courses/form-inscr-courses.component';
 import { FilterPipe } from './pipes/filter.pipe';
 import { ResultPipe } from './pipes/result.pipe';
-<<<<<<< HEAD
+
 import { CrudCourseComponent } from './crud-course/crud-course.component';
 import { UpdateCoursesComponent } from './update-courses/update-courses.component';
 import { CreateCourseComponent } from './create-course/create-course.component';
 import {  CustomDatePipe } from './pipes/custom-date.pipe';
-=======
+
 import { CrudStudentsComponent } from './crud/crud-students/crud-students.component';
 import { UpdateStudentsComponent } from './crud/update-students/update-students.component';
 import { CreateStudentComponent } from './crud/create-student/create-student.component';
->>>>>>> 540555e5af2d2f507fe9ad888ecd2277391f1e55
 
 
 
@@ -73,11 +72,8 @@ import { CreateStudentComponent } from './crud/create-student/create-student.com
                   PayTransfCourseComponent, 
                   AllCoursesComponent, 
                   FormInscrCoursesComponent, 
-<<<<<<< HEAD
                   FilterPipe, ResultPipe, CrudCourseComponent, UpdateCoursesComponent, CreateCourseComponent,  CustomDatePipe, 
-=======
-                  FilterPipe, ResultPipe, CrudStudentsComponent, UpdateStudentsComponent, CreateStudentComponent, 
->>>>>>> 540555e5af2d2f507fe9ad888ecd2277391f1e55
+                  CrudStudentsComponent, UpdateStudentsComponent, CreateStudentComponent, 
                 ],
   imports: [
     CommonModule,
