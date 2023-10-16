65e05cf445c0529fb1003ffc49a1e8f9ae94bff0
Ludmimar <ludmilamartos@gmail.com>
2023-10-16 09:30:25 -0300

    feat(frontend):Work in Login-Component in progress...

--- front/src/app/components/login/login.component.ts
+++ front/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/AuthService.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  constructor(private authService: AuthService) { }
 
+  login(user: string, pass: string) {
+    this.authService.login(user, pass).subscribe(response => {
+      // Aquí puedes manejar la respuesta del servidor, como guardar el token en local storage o redirigir a otra página.
+      console.log('Respuesta del servidor:', response);
+    });
+  }
 }
