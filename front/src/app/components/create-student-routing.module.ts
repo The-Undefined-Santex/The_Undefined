3d1343faade6839781e550d41204dd4c4297eac2
Ludmimar <ludmilamartos@gmail.com>
2023-10-18 13:43:33 -0300

    Delete coomments

--- front/src/app/components/create-student-routing.module.ts
+++ front/src/app/components/create-student-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UpdateStudentsComponent } from './crud/update-students/update-students.component';
 import { CreateStudentComponent } from './crud/create-student/create-student.component';
 
 const routes: Routes = [
