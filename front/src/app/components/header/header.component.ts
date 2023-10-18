0444666b9a21884cd62196ef29964ee084fe7303
Ludmimar <ludmilamartos@gmail.com>
2023-10-17 23:59:55 -0300

    Create login with AuthSerice

--- front/src/app/components/header/header.component.ts
+++ front/src/app/components/header/header.component.ts
@@ -2,19 +2,22 @@ import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UsersService } from 'src/app/services/users.service';
 import { AuthService } from 'src/app/services/AuthService.service';
-import { Student } from 'src/app/core/model/student.model';
 import { StudentsService } from 'src/app/services/students.service';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  providers: [MessageService]
 })
 export class HeaderComponent implements OnInit {
    nombreUsuario: string | null = '';  // Variable para almacenar el nombre de usuario
 
   visible: boolean = false;
 
+  
   showDialog() {
     this.visible = true;
   }
@@ -28,8 +31,11 @@ export class HeaderComponent implements OnInit {
       private formBuilder: FormBuilder,
       private cdr: ChangeDetectorRef,
       private authService: AuthService,
-      private studentModel: Student,
-      private studentService: StudentsService
+      private studentService: StudentsService,
+      private router: Router,
+      private messageService: MessageService,
+
+
     ) {
       this.form = this.formBuilder.group({
         userName: ['', [Validators.required]],
@@ -48,31 +54,61 @@ export class HeaderComponent implements OnInit {
 
   login(user: string, pass: string) {
     this.authService.login(user, pass).subscribe(response => {
-      this.visible = false;
-
+      console.log(response);
+      if (response && response.data) {
+      this.visible = false;       
+      const users = response.data.userName;
+      if(user !== users){
+        console.log("El usuario no existe");
+      }
       const userData = response.data;
-      const idUser = response.data.id;
-
-      localStorage.setItem('studentId', userData.id.toString());
-
-      this.studentService.getStudentById(+userData.id).subscribe(student => {
-        this.nombreUsuario = student.first_name; 
-
+      const rol = response.data.rol;
+      if (rol === "Student") {
+        this.router.navigate(['/platform-students']);
+        this.usuarioAutenticado = true; 
+      } else if (rol === "Admin") {
+        this.router.navigate(['/adminPlatform']);
         this.usuarioAutenticado = true;
-        this.userlogued = false;
-
-        this.cdr.detectChanges();
+      }
+      localStorage.setItem('studentId', userData.id.toString());
+      let login: string = "ok";
+      localStorage.setItem("login", login);
+      this.usuarioAutenticado = true;
+      this.userlogued = false;
+    } else {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: 'Usuario o contraseña incorrectos',
       });
-
+      this.visible = false;
+    }
     });
   }
+
+
+
   
-    Logout() {
-      localStorage.clear();
-      window.location.reload();
-    }
-  
-    get userName() {
+  Logout() {
+
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Deslogueo exitoso',
+      detail: `Redireccionando a la pagina principal`,
+    });
+
+    setTimeout(() => {
+      this.router.navigate(['/']);
+    }, 3000);
+
+    localStorage.clear();
+    this.usuarioAutenticado = false;
+    this.userlogued = true;
+  }
+
+
+
+    guserName() {
       return this.form.get("userName");
     }
   
